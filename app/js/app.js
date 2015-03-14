8332487c15da9232823a59def880e368751aaab5
Daniel Hernandez Zafra <daniel.hernandez.zafra@gmail.com>
2015-03-14 18:07:17 +0100

    ADD environment

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+/* App.js */
+angular.module('karts', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
+.config(['$routeProvider',
+  function($routeProvider) {
+  }
+])
+.run(['$rootScope', function($rootScope) {
+}]);
